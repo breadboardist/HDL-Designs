d49c794922de8727812f103327e96e54c9b53a2d
Prem Bharath <gadgetprem15@hotmail.com>
2015-02-12 22:32:44 -0800

    Tried 250 Mhz synthesis

---
+++ 287-250Mhz/INCA_libs/irun.lnx86.14.10.nc/.timestamp.ts
@@ -0,0 +1,2 @@
+1423808954 /home/so/soun0694/Work/287-250Mhz/tbsfilt.sv
+1423808859 /home/so/soun0694/Work/287-250Mhz/sfilt.v
---
+++ 287-250Mhz/INCA_libs/irun.lnx86.14.10.nc/files.ts
@@ -0,0 +1,3 @@
+1055114872 /apps/cadence/local/cdssetup/cds.lib
+1423808954 /home/so/soun0694/Work/287-250Mhz/tbsfilt.sv
+1423808859 /home/so/soun0694/Work/287-250Mhz/sfilt.v
