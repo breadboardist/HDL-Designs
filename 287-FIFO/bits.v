8e202a789e1dd4ba87f69c5d5b7a47a14a36321c
Prem Bharath <prembharath@outlook.com>
2015-02-20 17:02:32 -0800

    Dummy bits file

---
+++ 287-FIFO/bits.v
@@ -0,0 +1,22 @@
+module bits (
+	input clk,
+	input rst,
+	input pushin,
+	input datain,
+	input reqin,
+	input reqlen,
+	output pushout,
+	output lenout,
+	output dataout
+		
+);
+
+reg pushout
+
+always @(posedge clk)
+	begin
+		pushout<=0;
+	end
+	
+
+endmodule
