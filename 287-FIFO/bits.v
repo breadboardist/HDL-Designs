b1c1c63be2673abfe9ef6ccace75567208729af3
Prem Bharath <prembharath@outlook.com>
2015-02-20 18:39:07 -0800

    Bus sizes

--- 287-FIFO/bits.v
+++ 287-FIFO/bits.v
@@ -1,13 +1,13 @@
 module bits (
-	input clk,
-	input rst,
-	input pushin,
-	input [31:0] datain,
-	input reqin,
-	input reqlen,
-	output pushout,
-	output lenout,
-	output dataout
+	input 	clk,
+	input 	rst,
+	input 	pushin,
+	input 	[31:0] datain,
+	input 	reqin,
+	input 	[3:0] reqlen,
+	output 	pushout,
+	output	[3:0] lenout,
+	output  [14:0] dataout
 		
 );
 
