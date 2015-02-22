18fb751a5ed26e2720b8efca8738db66f81bedc9
Prem Bharath Soundararajan <gadgetprem15@hotmail.com>
2015-02-21 21:39:45 -0800

    New FIFO

--- 287-FIFO/bits.v
+++ 287-FIFO/bits.v
@@ -11,13 +11,14 @@ module bits (clock, reset, pushin, datain, reqin, reqlen, pushout, lenout, datao
 
 	reg pushout;
 
-	reg fifo_full;
+	//FIFO Signals
+	reg fifo_full, fifo_empty, fifo_last, fifo_slast, fifo_first, fifo_clear;
 	reg [31:0] fifo_input;
 	reg fifo_pop, fifo_push;
-	wire [31:0] fifo_output;
+	reg [31:0] fifo_output;
 	reg [31:0] fifo_output_reg;
 
-	fifo mainFIFO(clock, reset, fifo_input, fifo_push, fifo_pop, fifo_output, fifo_full);
+	FIFO fifo1(clock, reset, fifo_input, fifo_clear, fifo_push, fifo_pop, fifo_output, fifo_full, fifo_last, fifo_slast, fifo_first, fifo_empty);
 
 	always@(posedge clock)
 	begin
@@ -59,64 +60,253 @@ endmodule
 ///////////////////
 //FIFO Contoller //
 ///////////////////
-module fifo(clock, reset, inData, new_data, out_data, outData, full);
-	
-	input clock, reset;
-	input [31:0] inData;
-	//input [WIDTH-1:0] inData;
-	input new_data;
-	input out_data;
 
-	// parameter WIDTH = 32;
-	// parameter DEPTH = 32;
-	// parameter ADDRESSWIDTH = 5;
+`define  FWIDTH     32           // Width of the FIFO.
+`define  FDEPTH     32           // Depth of the FIFO.
+`define  FCWIDTH    5            // Counter Width of the FIFO 2 to power
+                                 // FCWIDTH = FDEPTH.
+module FIFO(  Clk,
+              RstN,
+              Data_In,
+              FClrN,
+              FInN,
+              FOutN,
 
-	integer k;
+              F_Data,
+              F_FullN,
+              F_LastN,
+              F_SLastN,
+              F_FirstN,
+              F_EmptyN
+           );
 
-	// output [WIDTH-1:0] outData;
-	output [31:0] outData;
-	output full;
 
-	reg full;
-	wire fullD;
 
-	// reg [ADDRESSWIDTH-1:0] rear;
-	// reg [ADDRESSWIDTH-1:0] front;
-	reg [4:0] rear;
-	reg [4:0] front;
+input                       Clk;      // CLK signal.
+input                       RstN;     // Reset signal.
+input [(`FWIDTH-1):0]       Data_In;  // Data into FIFO.
+input                       FInN;     // Write into FIFO Signal.
+input                       FClrN;    // Clear signal to FIFO.
+input                       FOutN;    // Read from FIFO signal.
 
-	//Rear Pointer Holder Flops
-	always@(posedge clock)
-	begin
-		if(!reset) rear <= 0;
-		else if (new_data)
-		begin
-			if(rear == 32) rear <= 0;
-			else rear <= rear + 1;
-		end
-	end
+output [(`FWIDTH-1):0]      F_Data;   // FIFO data out.
+output                      F_FullN;  // FIFO full indicating signal.
+output                      F_EmptyN; // FIFO empty indicating signal.
+output                      F_LastN;  // FIFO Last but one signal.
+output                      F_SLastN; // FIFO SLast but one signal.
+output                      F_FirstN; // Signal indicating only one
+                                      // word in FIFO.
 
-	//Front Pointer Holder Flops
-	always@(posedge clock)
-	begin
-		if(!reset) front <= 0;
-		else if(out_data)
-		begin
-			if(front == 32) front <= 0;
-			else front <= front + 1;
-		end
-	end
 
-	//Full Signal Flop
-	always@(posedge clock)
-	begin
-		if(!reset) full <= 0;
-		else full <= fullD;
-	end
+reg                F_FullN;
+reg                F_EmptyN;
+reg                F_LastN;
+reg                F_SLastN;
+reg                F_FirstN;
 
-	assign fullD = (front == ((rear == 32) ? 0: (rear + 1)));
+reg    [`FCWIDTH:0]      fcounter; //counter indicates num of data in FIFO
+reg    [(`FCWIDTH-1):0]   rd_ptr;      // Current read pointer.
+reg    [(`FCWIDTH-1):0]   wr_ptr;      // Current write pointer.
+wire   [(`FWIDTH-1):0]    FIFODataOut; // Data out from FIFO MemBlk
+wire   [(`FWIDTH-1):0]    FIFODataIn;  // Data into FIFO MemBlk
 
-	regfile u1(clock, reset, new_data, rear, front, inData, outData);
+wire   ReadN  = FOutN;
+wire   WriteN = FInN;
+
+assign F_Data     = FIFODataOut;
+assign FIFODataIn = Data_In;
+
+
+    FIFO_MEM_BLK memblk(.clk(Clk),
+                        .writeN(WriteN),
+                        .rd_addr(rd_ptr),
+                        .wr_addr(wr_ptr),
+                        .data_in(FIFODataIn),
+                        .data_out(FIFODataOut)
+                       );
+
+
+
+    // Control circuitry for FIFO. If reset or clr signal is asserted,
+    // all the counters are set to 0. If write only the write counter
+    // is incremented else if read only read counter is incremented
+    // else if both, read and write counters are incremented.
+    // fcounter indicates the num of items in the FIFO. Write only
+    // increments the fcounter, read only decrements the counter, and
+    // read && write doesn't change the counter value.
+    always @(posedge Clk or posedge RstN)
+    begin
+
+       if(RstN) begin
+           fcounter    <= 0;
+           rd_ptr      <= 0;
+           wr_ptr      <= 0;
+       end
+       else begin
+
+           if(!FClrN ) begin
+               fcounter    <= 0;
+               rd_ptr      <= 0;
+               wr_ptr      <= 0;
+           end
+           else beginx`
+
+               if(!WriteN && F_FullN)
+                   wr_ptr <= wr_ptr + 1;
+
+               if(!ReadN && F_EmptyN)
+                   rd_ptr <= rd_ptr + 1;
+
+               if(!WriteN && ReadN && F_FullN)
+                   fcounter <= fcounter + 1;
+
+               else if(WriteN && !ReadN && F_EmptyN)
+                   fcounter <= fcounter - 1;
+          end
+       end
+    end
+
+
+
+    // All the FIFO status signals depends on the value of fcounter.
+    // If the fcounter is equal to fdepth, indicates FIFO is full.
+    // If the fcounter is equal to zero, indicates the FIFO is empty.
+
+
+
+    // F_EmptyN signal indicates FIFO Empty Status. By default it is
+    // asserted, indicating the FIFO is empty. After the First Data is
+    // put into the FIFO the signal is deasserted.
+    always @(posedge Clk or posedge RstN)
+    begin
+
+       if(RstN)
+          F_EmptyN <= 1'b0;
+
+       else begin
+          if(FClrN==1'b1) begin
+
+             if(F_EmptyN==1'b0 && WriteN==1'b0)
+
+                 F_EmptyN <= 1'b1;
+
+             else if(F_FirstN==1'b0 && ReadN==1'b0 && WriteN==1'b1)
+
+                 F_EmptyN <= 1'b0;
+          end
+          else
+             F_EmptyN <= 1'b0;
+       end
+    end
+
+    // F_FirstN signal indicates that there is only one datum sitting
+    // in the FIFO. When the FIFO is empty and a write to FIFO occurs,
+    // this signal gets asserted.
+    always @(posedge Clk or posedge RstN)
+    begin
+
+       if(RstN)
+
+          F_FirstN <= 1'b1;
+
+       else begin
+          if(FClrN==1'b1) begin
+
+             if((F_EmptyN==1'b0 && WriteN==1'b0) ||
+                (fcounter==2 && ReadN==1'b0 && WriteN==1'b1))
+
+                 F_FirstN <= 1'b0;
+
+             else if (F_FirstN==1'b0 && (WriteN ^ ReadN))
+                 F_FirstN <= 1'b1;
+          end
+          else begin
+
+             F_FirstN <= 1'b1;
+          end
+       end
+    end
+
+
+    // F_SLastN indicates that there is space for only two data words
+    //in the FIFO.
+    always @(posedge Clk or posedge RstN)
+    begin
+
+       if(RstN)
+
+          F_SLastN <= 1'b1;
+
+       else begin
+
+          if(FClrN==1'b1) begin
+
+             if( (F_LastN==1'b0 && ReadN==1'b0 && WriteN==1'b1) ||
+                 (fcounter == (`FDEPTH-3) && WriteN==1'b0 && ReadN==1'b1))
+
+                 F_SLastN <= 1'b0;
+
+
+             else if(F_SLastN==1'b0 && (ReadN ^ WriteN) )
+                 F_SLastN <= 1'b1;
+
+          end
+          else
+             F_SLastN <= 1'b1;
+
+       end
+    end
+
+    // F_LastN indicates that there is one space for only one data
+    // word in the FIFO.
+    always @(posedge Clk or posedge RstN)
+    begin
+
+       if(RstN)
+
+          F_LastN <= 1'b1;
+
+       else begin
+          if(FClrN==1'b1) begin
+
+             if ((F_FullN==1'b0 && ReadN==1'b0)  ||
+                 (fcounter == (`FDEPTH-2) && WriteN==1'b0 && ReadN==1'b1))
+
+                 F_LastN <= 1'b0;
+
+             else if(F_LastN==1'b0 && (ReadN ^ WriteN) )
+                 F_LastN <= 1'b1;
+          end
+          else
+             F_LastN <= 1'b1;
+       end
+    end
+
+
+    // F_FullN indicates that the FIFO is full.
+    always @(posedge Clk or posedge RstN)
+    begin
+
+       if(RstN)
+
+           F_FullN <= 1'b1;
+
+       else begin
+           if(FClrN==1'b1)  begin
+
+               if (F_LastN==1'b0 && WriteN==1'b0 && ReadN==1'b1)
+
+                    F_FullN <= 1'b0;
+
+               else if(F_FullN==1'b0 && ReadN==1'b0)
+
+                    F_FullN <= 1'b1;
+           end
+           else
+               F_FullN <= 1'b1;
+
+       end
+    end
 
 endmodule
 
@@ -125,47 +315,34 @@ endmodule
 ///////////////////////////////////
 //Memory array for use with FIFO //
 ///////////////////////////////////
-module regfile(clock, reset, writeEnable, dest, source, dataIn, dataOut);
-	
-	parameter WIDTH = 32;
-	parameter DEPTH = 32;
-	parameter ADDRESSWIDTH = 5;
-	integer i,j;
-
-	input clock, reset, writeEnable;
-	input [ADDRESSWIDTH-1 : 0] dest; 
-	input [ADDRESSWIDTH-1 : 0] source; 
-	input [WIDTH-1 : 0] dataIn; 
-	output [WIDTH-1 : 0] dataOut;
-
-	reg [WIDTH-1 : 0] dataOut; // registered output
-	reg [WIDTH-1 : 0] rf [DEPTH-1 : 0]; 
-	wire [DEPTH-1 : 0] writeEnableDecoded;
-
-	assign writeEnableDecoded = (writeEnable << dest);
-
-	// flip-flop for data-out 
-	always@(posedge clock)
-		begin
-			if(!reset) dataOut <= 0;
-			else dataOut <= rf[source]; 
-		end
-
-	//memory array
-	always@(posedge clock)
-	begin
-		if(!reset)
-		begin
-			for(i = 0; i<DEPTH; i = i+1)
-				rf[i] <= 0;
-		end 
-		else
-		begin
-			for(j = 0; j<DEPTH; j = j+1)
-				if(writeEnableDecoded[j]) rf[j] <= dataIn;
-		end
-	end
+module FIFO_MEM_BLK( clk,
+                     writeN,
+                     wr_addr,
+                     rd_addr,
+                     data_in,
+                     data_out
+                   );
 
 
+input                    clk;       // input clk.
+input  writeN;  // Write Signal to put data into fifo.
+input  [(`FCWIDTH-1):0]  wr_addr;   // Write Address.
+input  [(`FCWIDTH-1):0]  rd_addr;   // Read Address.
+input  [(`FWIDTH-1):0]   data_in;   // DataIn in to Memory Block
+
+output [(`FWIDTH-1):0]   data_out;  // Data Out from the Memory
+                                    // Block(FIFO)
+
+wire   [(`FWIDTH-1):0] data_out;
+
+reg    [(`FWIDTH-1):0] FIFO[0:(`FDEPTH-1)];
+assign data_out  = FIFO[rd_addr];
+
+always @(posedge clk)
+begin
+
+   if(writeN==1'b0)
+      FIFO[wr_addr] <= data_in;
+end
 
 endmodule
