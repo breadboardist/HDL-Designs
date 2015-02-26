5d50a17af58fdb501ff935a261fe16a0c2f30313
Prem Bharath <gadgetprem15@hotmail.com>
2015-02-26 14:40:59 -0800

    Bits run completed :)

--- 287-FIFO/INCA_libs/irun.lnx86.14.10.nc/files.ts
+++ 287-FIFO/INCA_libs/irun.lnx86.14.10.nc/files.ts
@@ -1,4 +1,4 @@
 1055114872 /apps/cadence/local/cdssetup/cds.lib
-1424986203 /home/so/soun0694/Work/287-FIFO/tbits.sv
-1424986136 /home/so/soun0694/Work/287-FIFO/bits.v
-1424986203 /home/so/soun0694/Work/287-FIFO/bits_gates.v
+1424990380 /home/so/soun0694/Work/287-FIFO/tbits.sv
+1424990310 /home/so/soun0694/Work/287-FIFO/bits.v
+1424990380 /home/so/soun0694/Work/287-FIFO/bits_gates.v
--- 287-FIFO/tbits.sv
+++ 287-FIFO/tbits.sv
@@ -21,7 +21,7 @@ int bitsleft;
 
 reg running=0;
 
-reg debug=0;
+reg debug=1;
 
 default clocking tclk @(posedge(clk)) ;
   
