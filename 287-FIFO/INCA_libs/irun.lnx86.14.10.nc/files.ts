0f8c2ee10969718259dde519c14b15349164a781
Prem Bharath <gadgetprem15@hotmail.com>
2015-02-26 15:06:33 -0800

    Bits run fresh. Almost ready to submit

--- 287-FIFO/INCA_libs/irun.lnx86.14.10.nc/files.ts
+++ 287-FIFO/INCA_libs/irun.lnx86.14.10.nc/files.ts
@@ -1,4 +1,4 @@
 1055114872 /apps/cadence/local/cdssetup/cds.lib
-1424990380 /home/so/soun0694/Work/287-FIFO/tbits.sv
-1424990310 /home/so/soun0694/Work/287-FIFO/bits.v
-1424990380 /home/so/soun0694/Work/287-FIFO/bits_gates.v
+1424991883 /home/so/soun0694/Work/287-FIFO/tbits.sv
+1424991658 /home/so/soun0694/Work/287-FIFO/bits.v
+1424991883 /home/so/soun0694/Work/287-FIFO/bits_gates.v
