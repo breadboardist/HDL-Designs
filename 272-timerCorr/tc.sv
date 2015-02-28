b376c0835e9d8009809f4a51a5ce9b51c0729507
Prem Bharath Soundararajan <gadgetprem15@hotmail.com>
2015-02-27 19:01:47 -0800

    Read error rectification

--- 272-timerCorr/tc.sv
+++ 272-timerCorr/tc.sv
@@ -49,25 +49,23 @@ module tc (tci ai);
 			end
 			//Read data
 			else if (R_timsk==7 && ai.read==1) begin
-				case (ai.addr)
-					8'h27: ai.rdata <= R_ocra;
-					8'h28: ai.rdata <= R_ocrb;
-					8'h24: ai.rdata <= R_tccra;
-					8'h25: ai.rdata <= R_tccrb;
-					8'h15: ai.rdata <= R_tifr;
-					8'h6E: ai.rdata <= R_timsk;
-					8'h47: ai.rdata <= R_ocra; 
-					8'h44: ai.rdata <= R_tccra;
-					8'h15: ai.rdata <= 8'h06;
-					8'h35: ai.rdata <= 8'h06;
-					8'h45: ai.wdata <= R_tccrb1;
-					8'h26: 	begin
-								R_tcnt <= R_tccra; 
-								ai.rdata <= R_tcnt; 
-								repeat(4) @(posedge (ai.clk)); ai.rdata <= R_tcnt+1'h1;
-								ai.interrupt_request <= 1;
-							end
-				endcase
+					if (hw1.addr==8'h27) hw1.rdata <= R_ocra;
+					if (hw1.addr==8'h28) hw1.rdata <= R_ocrb;
+					if (hw1.addr==8'h24) hw1.rdata <= R_tccra;
+					if (hw1.addr==8'h25) hw1.rdata <= R_tccrb;
+					if (hw1.addr==8'h15) hw1.rdata <= R_tifr;
+					if (hw1.addr==8'h6E) hw1.rdata <= R_timsk;
+					if (hw1.addr==8'h47) hw1.rdata <= R_ocra; 
+					if (hw1.addr==8'h44) hw1.rdata <= R_tccra;
+					if (hw1.addr==8'h15) hw1.rdata <= 8'h06;
+					if (hw1.addr==8'h35) hw1.rdata <= 8'h06;
+					if (hw1.addr==8'h45) hw1.wdata <= R_tccrb1;
+					if (hw1.addr==8'h26) begin
+											R_tcnt <= R_tccra; 
+											hw1.rdata <= R_tcnt; 
+											repeat(4) @(posedge (hw1.clk)); hw1.rdata <= R_tcnt+1'h1;
+											hw1.interrupt_request <= 1;
+										end
 			end
 
 			else if (R_tcnt==2 && ai.write==1) begin
