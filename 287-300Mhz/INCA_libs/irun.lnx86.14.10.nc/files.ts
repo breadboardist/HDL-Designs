37cddfaebdfa2512dbff258b24a51489e930044c
Prem Bharath Soundararajan <gadgetprem15@hotmail.com>
2015-02-28 23:51:25 -0800

    Moving back to 2 files

--- 287-300Mhz/INCA_libs/irun.lnx86.14.10.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1055114872 /apps/cadence/local/cdssetup/cds.lib
-1425195211 /home/so/soun0694/Work/287-300Mhz/tbsfilt.sv
-1425195085 /home/so/soun0694/Work/287-300Mhz/sfilt.v
-1425195211 /home/so/soun0694/Work/287-300Mhz/sfilt_gates.v
--- 287-300Mhz/INCA_libs/irun.lnx86.14.10.nc/ydir_files.ts
+++
@@ -1,1 +0,0 @@
-1155151579 /apps/toshiba/sjsu/verilog/tc240c
