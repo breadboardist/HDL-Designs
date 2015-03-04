dd027fe283ad9bff1ced696acb1ac402c7943c1e
Prem Bharath <gadgetprem15@hotmail.com>
2015-03-03 23:39:45 -0800

    Delay Mux minor modification

--- 271-HW/INCA_libs/irun.lnx86.14.10.nc/.timestamp.ts
+++ 271-HW/INCA_libs/irun.lnx86.14.10.nc/.timestamp.ts
@@ -1,4 +1,4 @@
 1425446384 /home/so/soun0694/Work/271-HW/tr_latch.v
 1425445969 /home/so/soun0694/Work/271-HW/problem_2.v
 1425447054 /home/so/soun0694/Work/271-HW/adder.v
-1425447054 /home/so/soun0694/Work/271-HW/delay_mux.v
+1425454674 /home/so/soun0694/Work/271-HW/delay_mux.v
