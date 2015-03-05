fb8e103c3afbe988d7973b625e34254b5f6a3fd7
Prem Bharath <gadgetprem15@hotmail.com>
2015-03-05 15:58:13 -0800

    PnR HW Completed

---
+++ 287-PnR/poly5.enc.dat/poly5_power_constraints.tcl
@@ -0,0 +1,1 @@
+set_power_analysis_mode -create_binary_db false -handle_glitch false -disable_static false -handle_tri_state false -honor_negative_energy true -ignore_control_signals true -ignore_inout_pin_cap false -state_dependent_leakage true -method static -split_bus_power false -transition_time_method max -x_transition_factor 0.500000 -z_transition_factor 0.250000
