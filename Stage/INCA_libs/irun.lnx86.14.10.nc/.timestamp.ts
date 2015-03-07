ee10d2eda36c0e35d29b72e4cb54019656b81f36
Prem Bharath <gadgetprem15@hotmail.com>
2015-03-06 16:37:36 -0800

    ALU untested

--- Stage/INCA_libs/irun.lnx86.14.10.nc/.timestamp.ts
+++ Stage/INCA_libs/irun.lnx86.14.10.nc/.timestamp.ts
@@ -1,1 +1,1 @@
-1425687828 /home/so/soun0694/Work/Stage/alu.v
+1425688308 /home/so/soun0694/Work/Stage/alu.v
--- Stage/INCA_libs/irun.lnx86.14.10.nc/files.ts
+++ Stage/INCA_libs/irun.lnx86.14.10.nc/files.ts
@@ -1,2 +1,2 @@
 1055114872 /apps/cadence/local/cdssetup/cds.lib
-1425687828 /home/so/soun0694/Work/Stage/alu.v
+1425688308 /home/so/soun0694/Work/Stage/alu.v
