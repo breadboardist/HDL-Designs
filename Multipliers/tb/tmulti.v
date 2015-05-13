100b75040105282ba36b192f3bc0a20bb623c53d
Prem Bharath Soundararajan <gadgetprem15@hotmail.com>
2015-05-13 10:54:36 -0700

    Modified tb for multiplier

--- Multipliers/tb/tmulti.v
+++ Multipliers/tb/tmulti.v
@@ -30,16 +30,16 @@ module tmulti();
   //Ports and register declarations //
   ////////////////////////////////////
   
-  reg clock, reset, start, start_bf;
-  reg [31:0] mlier, mcand, r_mlier, r_mcand; 
+  reg clock, reset, start, start_d;
+  reg [31:0] mlier, mcand, loadedmlier, loadedmcand; 
   reg [63:0] r_prodt, c_prodt; 
   wire [63:0] prodt;
   wire valid;
 
-  wire [31:0] q0,h0;
-  wire [63:0] mr1,mr2,exp;
+  wire [31:0] multiplier, multiplicand;
+  wire [63:0] multiplied,comp_multiplied,exp;
   reg  [5 :0] pt;
-  reg  [7 :0] latency, r_latency, tmp1;
+  reg  [7 :0] latency, latency_q, loadwait;
   reg debug = 1 ;
 
   initial begin
@@ -201,40 +201,40 @@ end
 generate
   if (tb_type == 1 || tb_type == 2) begin : fix_latency_enable
     multi    multi   (clock, reset, mlier, mcand, prodt, start, valid);
-  end else if (tb_type == 3 || tb_type == 4) begin : var_latency_enable
+  end else if (tb_type == 3 || tb_type == 4) begin : valatency_q_enable
     multi_vl multi_vl(clock, reset, mlier, mcand, prodt, start, valid);
   end
 endgenerate
 
 
-assign  q0 = (r_mlier[31])? (~r_mlier + 1'b1) : r_mlier;
-assign  h0 = (r_mcand[31])? (~r_mcand + 1'b1) : r_mcand;
-assign  mr1= q0*h0;
-assign  mr2= ~(mr1-1'b1);
-assign  exp= ((r_mlier[31]^r_mcand[31])&& (|mr1)) ? {1'b1,mr2} : {1'b0,mr1};
+assign  multiplier = (loadedmlier[31])? (~loadedmlier + 1'b1) : loadedmlier;
+assign  multiplicand = (loadedmcand[31])? (~loadedmcand + 1'b1) : loadedmcand;
+assign  multiplied = multiplier* multiplicand;
+assign  comp_multiplied = ~(multiplied-1'b1);
+assign  exp = ((loadedmlier[31]^loadedmcand[31])&& (|multiplied)) ? {1'b1,comp_multiplied} : {1'b0,multiplied};
 
 always @( posedge clock or posedge reset ) begin
   if (reset) begin
-    pt <= 1; latency <= 0; r_latency <= 0; tmp1 <= 0;
-    r_prodt <= 0; r_mlier <= 0; r_mcand <= 0; start_bf <= 0;
+    pt <= 1; latency <= 0; latency_q <= 0; loadwait <= 0;
+    r_prodt <= 0; loadedmlier <= 0; loadedmcand <= 0; start_d <= 0;
   end else begin
-    if (tmp1[4]) begin 
-	r_latency <= 0;
+    if (loadwait[4]) begin 
+	latency_q <= 0;
     end else if (valid) begin
-   	r_latency <= latency;
+   	latency_q <= latency;
     end
 
     if (valid) begin
-        tmp1 <= 8'b1; 
-    end else if (tmp1[7]) begin
-        tmp1 <= 8'b0; 
-    end else if (tmp1 != 0) begin
-        tmp1 <= {tmp1[6:0],tmp1[7]}; 
+        loadwait <= 8'b1; 
+    end else if (loadwait[7]) begin
+        loadwait <= 8'b0; 
+    end else if (loadwait != 0) begin
+        loadwait <= {loadwait[6:0],loadwait[7]}; 
     end
 
-    start_bf <= start;
+    start_d <= start;
    
-    if(start && !start_bf)begin
+    if(start && !start_d)begin
       latency <= 1; 
     end else if (valid)begin
       latency <= 0; 
@@ -242,49 +242,47 @@ always @( posedge clock or posedge reset ) begin
       latency <= latency + 1; 
     end 
 
-    if(start && !start_bf)begin
-      r_mcand <= mcand; 
-      r_mlier <= mlier; 
+    if(start && !start_d)begin
+      loadedmcand <= mcand; 
+      loadedmlier <= mlier; 
     end
 
     if (latency > 33) begin
-        error(4'h2, pt, prodt, exp);
+        error(1'b0, pt, prodt, exp);
     end 
 
     if (valid) begin
       r_prodt <= prodt;
       c_prodt <= (prodt[63]) ? ~(prodt-1) : prodt;
       pt <= pt + 1;
-      $display("=> Test Pattern #%d: Multiplier=32'h%h, Multiplcand=32'h%h", pt, r_mlier, r_mcand);
-      $display("=> Expected product is  : 64'h%h", exp);
-      $display("=> Computed product is  : 64'h%h", prodt);
-      $display("=> Latency is           : %d\n", latency);
+      $display("--> Test Pattern #%d: Multiplier=32'h%h * Multiplicand=32'h%h", pt, loadedmlier, loadedmcand);
+      $display("--> Expected product is  : 64'h%h", exp);
+      $display("--> Computed product is  : 64'h%h", prodt);
+      $display("--> Latency is           : %d\n", latency);
       if (prodt != exp) begin
-        error(4'h1, pt, prodt, exp);
+        error(1'b1, pt, prodt, exp);
       end
     end        
   end
 end
 
-task error(input [3:0] status, pt, input [63:0] prodt, exp);
+task error(input status, pt, input [63:0] prodt, exp);
   begin
-    $display("-=-=-=-=-=-=-=-=-ERROR Message-=-=-=-=-=-=-=-=-=-");
+    $display("Error");
     $display("\n");
     $display("\n");
-
-    if (status == 4'h1) begin
-      $display("Data No.%d dose not match", pt);
-      $display("Expect data is %h", exp);
-      $display("Product is     %h", prodt);
-    end else if (status == 4'h2) begin
-      $display("no valid output is detected in 33 clock");
-      $display("Expect data is %h", exp);
-      $display("Product is     %h", prodt);
+    if (status == 1'b1) begin
+      $display("Answer Mismatch");
+      $display("Expected output is %h", exp);
+      $display("Produced output is %h", prodt);
+    end else if (status == 1'b0) begin
+      $display("Took longer than 33 cycles");
+      $display("Expected output is %h", exp);
+      $display("Produced output is %h", prodt);
     end  
-    
     $display("\n");
     $display("\n");
-    $display("-=-=-=-=-=-=-=-=-     End    -=-=-=-=-=-=-=-=-=-");
+    $display("End");
     #200;
     $finish;
   end
